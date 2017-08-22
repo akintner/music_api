86141789720f74358b2f48799f8d663c5c70a88b
Amz K <akintner@u.rochester.edu>
2017-08-22 10:26:35 -0600

    finalize years counting

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,9 @@
 class Album < ApplicationRecord
+
+  def self.most_albums_by_year(year)
+    count_by_year = Album.select(:year).order("year")
+    num = count_by_year.where(year: year.to_s).count
+    num
+  end
+
 end
